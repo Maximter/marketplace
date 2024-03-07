726cf71eb9608889da62c1da325fae965b06be66
Maxim <m-a-x-o-k@yandex.ru>
2024-03-07 14:46:18 +0300

    create first page

--- marketplace/homepage/views.py
+++ marketplace/homepage/views.py
@@ -3,5 +3,5 @@ from django.shortcuts import render
 # Create your views here.
 
 def index(request):
-    context = {'message': 'Привет, мир!'}
-    return render(request,  '../homepage/templates/homepage/index.html', context)
+    context = {'name': 'Maxim'}
+    return render(request,  'homepage/index.html', context)
