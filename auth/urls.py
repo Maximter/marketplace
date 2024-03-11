d593a17fe5c5bebbeffde7b091fe6ba46e14d8e6
Maxim <m-a-x-o-k@yandex.ru>
2024-03-11 16:59:27 +0300

    start auth

--- auth/urls.py
+++ auth/urls.py
@@ -14,10 +14,12 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
-from django.contrib import admin
 from django.urls import path
+from django.contrib.auth import views as auth_views
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='home')
+    path('', views.index, name='auth'),
+    path('login/', auth_views.LoginView.as_view(), name='login'),
+    path('logout/', auth_views.LogoutView.as_view(), name='logout'),
 ]
