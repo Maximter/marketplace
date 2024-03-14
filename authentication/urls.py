d7bec6b3009ae048c04fa78646940c0f6df49d9b
Maxim <m-a-x-o-k@yandex.ru>
2024-03-14 13:35:36 +0300

    finished login and signup

--- authentication/urls.py
+++ authentication/urls.py
@@ -21,6 +21,6 @@ from . import views
 
 urlpatterns = [
     path('register/', views.register, name='register'),
-    path('login/', auth_views.LoginView.as_view(template_name='auth.html'), name='login'),
+    path('login/', auth_views.LoginView.as_view(template_name='auth.html', next_page='home'), name='login'),
     path('logout/', auth_views.LogoutView.as_view(next_page='login'), name='logout'),
 ]
