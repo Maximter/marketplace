e5fb7cd4e83d7aed8cbedba4ffae4e570454ae6e
Maxim <m-a-x-o-k@yandex.ru>
2024-03-14 14:13:52 +0300

    update gitignore

--- homepage/views.py
+++
@@ -1,7 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-
-def index(request):
-    context = {'name': 'Maxim'}
-    return render(request,  'homepage.html', context)
--- user/views.py
+++
@@ -1,6 +0,0 @@
-from django.shortcuts import render
-
-# Create your views here.
-def index(request):
-    context = {'name': 'Maxim'}
-    return render(request,  'user.html', context)
