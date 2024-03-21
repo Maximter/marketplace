8bd54c6e8c4e37ddf3caa3e3125915f291aeaf2a
Maxim <m-a-x-o-k@yandex.ru>
2024-03-21 13:14:02 +0300

    create seller account

--- seller/views.py
+++ seller/views.py
@@ -1,26 +1,27 @@
 from django.shortcuts import get_object_or_404, render, redirect
 from django.contrib.auth.decorators import login_required, permission_required
 from django.contrib.contenttypes.models import ContentType
+from authentication.decorators import group_required
 from authentication.models import CustomUser
 from django.contrib.auth.models import Permission
+from django.contrib.auth.models import Group
 
 # Create your views here.
-@permission_required("authentication.may_sell", raise_exception=True)
+@group_required('Sellers')
 def index(request):
     context = {'name': request.user.first_name}
     return render(request, 'seller.html', context)
 
 @login_required
 def create(request):
+    if request.user.groups.filter(name='Sellers').exists():
+        return redirect('seller')
+
     if request.method == 'POST':
         user = request.user
-        content_type = ContentType.objects.get_for_model(CustomUser)
         try:
-            permission = Permission.objects.get(
-                codename="may_sell",
-                content_type=content_type,
-            )
-            user.user_permissions.add(permission)
+            sellers_group = Group.objects.get(name='Sellers')
+            user.groups.add(sellers_group)
         except Permission.DoesNotExist:
             print("Permission doesn't exist")
         return redirect('seller')  
--- user/views.py
+++ user/views.py
@@ -3,5 +3,5 @@ from django.contrib.auth.decorators import login_required
 
 @login_required
 def index(request):
-    context = {'name': request.user.first_name}
+    context = {'name': request.user.first_name, 'seller': request.user.groups.filter(name='Sellers').exists()}
     return render(request,  'user.html', context)
