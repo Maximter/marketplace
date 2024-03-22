6aa188fe72bb2a51b8927b0be776cf993035e4be
Maxim <m-a-x-o-k@yandex.ru>
2024-03-22 10:01:46 +0300

    update admin panel

--- category/admin.py
+++ category/admin.py
@@ -1,3 +1,10 @@
 from django.contrib import admin
 
 # Register your models here.
+from django.contrib import admin
+from .models import Category
+
+@admin.register(Category)
+class CategoryAdmin(admin.ModelAdmin):
+    list_display = ['name', 'cover']
+    search_fields = ['name']
