0412bc9d46294cee1a3517ec2725f3f2be07ead4
Maxim <m-a-x-o-k@yandex.ru>
2024-03-22 09:31:04 +0300

    configured url

--- product/urls.py
+++ product/urls.py
@@ -15,13 +15,10 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import include, path
+from django.urls import path
+from . import views
 
 urlpatterns = [
-    path('', include('homepage.urls')),
-    path('auth/', include('authentication.urls')),
-    path('user/', include('user.urls')),
-    path('seller/', include('seller.urls')),
-    path('product/', include(('product.urls', 'product'), namespace='product')),
-    path('admin/', admin.site.urls),
+    path('', views.index, name='product'),
+    path('create/', views.create, name='create')
 ]
--- product/views.py
+++ product/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
 
+from authentication.decorators import group_required
+
 # Create your views here.
+
+def index(request):
+    context = {'name': request.user.first_name}
+    return render(request, 'product.html', context)
+
+@group_required('Sellers')
+def create(request):
+    if request.method == 'POST':
+        user = request.user
+        # try:
+        #     sellers_group = Group.objects.get(name='Sellers')
+        #     user.groups.add(sellers_group)
+        # except Permission.DoesNotExist:
+        #     print("Permission doesn't exist")
+        # return redirect('seller')  
+
+    context = {'name': request.user.first_name}
+    return render(request, 'create_product.html', context)
