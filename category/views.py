cb1e90eadf29836bcd94f3cebbeaa2146e54b718
Maxim <m-a-x-o-k@yandex.ru>
2024-03-22 10:40:13 +0300

    category pages

--- category/views.py
+++ category/views.py
@@ -1,5 +1,16 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+
+from category.models import Category
 
 # Create your views here.
 def index(request):
-    return render(request, 'category.html')
+    categories = Category.objects.all()
+    return render(request, 'all_categories.html', {'categories': categories })
+
+def category(request, category_name):
+    try:
+        category = Category.objects.get(name=category_name)
+    except Category.DoesNotExist:
+        return redirect('home')
+    
+    return render(request, 'category.html', {'category': category})
--- marketplace/urls.py
+++ marketplace/urls.py
@@ -16,6 +16,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('', include('homepage.urls')),
@@ -25,4 +27,4 @@ urlpatterns = [
     path('product/', include(('product.urls', 'product'), namespace='product')),
     path('category/', include(('category.urls', 'category'), namespace='category')),
     path('admin/', admin.site.urls),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
