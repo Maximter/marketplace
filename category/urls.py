e862ac9fe6f06f84ab053073e8bd7416ff1bfa40
Maxim <m-a-x-o-k@yandex.ru>
2024-03-22 10:40:13 +0300

    category pages

--- category/urls.py
+++ category/urls.py
@@ -19,5 +19,6 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.index, name='category')
+    path('', views.index, name='home'),
+    path('<str:category_name>/', views.category, name='category')
 ]
